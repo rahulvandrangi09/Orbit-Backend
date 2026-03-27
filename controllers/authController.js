0eae703dd945b955381cfdc13c6f47fc91f900b2
rahulvandrangi09 <rahulvandrangi1k@gmail.com>
2026-03-27 14:35:11 +0530

    updated mail transporter

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,9 @@ import nodemailer from "nodemailer";
 const prisma = new PrismaClient();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
